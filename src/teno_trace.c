0e7dd4a5bc29476fd3754a5df4306b306332d29a
terry3 <tfguo369@gmail.com>
2015-07-31 13:21:10 +0800

    Revert conficlts.

--- src/teno_trace.c
+++ src/teno_trace.c
@@ -6,13 +6,8 @@
 #include "teno_cli.h"
 #include "teno_trace.h"
 
-<<<<<<< HEAD
 /* Trace the message */
 /* Just print now, will modified to enhanced. */
-=======
-/* trace the message */
-/* just print now, will modified to enhanced. */
->>>>>>> f7e919cd6cd80271e7cba7cddcf079203eb032b1
 T_VOID teno_msg_trace(T_MSG *ps_msg) {
     PN_RET_N(ps_msg);
     printf("fs=[%d], ts=[%d], fp=[%d], tp=[%d], T=[%d], L=[%d]\n",
