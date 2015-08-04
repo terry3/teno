9c73da7cdb2559cbe3b9bb72e19fac7adb57e409
terry3 <tfguo369@gmail.com>
2015-08-04 11:07:58 +0800

    Delete some .o file, add socket feature.

---
+++ example/example_2_client.c
@@ -0,0 +1,17 @@
+#include "teno.h"
+#include "teno_pub.h"
+#include "teno_mq.h"
+#include "teno_service.h"
+#include "teno_monitor.h"
+#include "teno_msg.h"
+
+extern F_RET teno_client();
+
+int main() {
+    teno_client();
+    return 0;
+}
+
+
+
+
